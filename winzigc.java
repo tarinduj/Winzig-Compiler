cc6bcaa95ec7e36881a05a117da4dd46acb06d05
Tarindu Jayatilaka <tarindujayatilaka@gmail.com>
2021-03-13 23:35:21 +0530

    merged

--- winzigc.java
+++ winzigc.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 public class winzigc{
     public static void main(String[] args){
         
-        Lexer lexer = new Lexer();
-        Parser parser  = new Parser();
-        CodeGenerator codegen = new CodeGenerator();
+        Lexer lexer = new Lexer(); //tokenizer
+        Parser parser  = new Parser(); //syntax analysis
+        CodeGenerator codegen = new CodeGenerator(); //semantic analysis and code geneneration
 
         try{
             String flag = args[0];
@@ -16,13 +16,16 @@ public class winzigc{
                 ArrayList<Token> scannedTokens = lexer.scan(inputPath); 
                 ArrayList<Token> screenedTokens = lexer.screen(scannedTokens);
 
+                // to get the tokens
                 if (flag.equals("-tokens")){
                     for (Token token : screenedTokens) {
                         System.out.println(token.tokenType + " -> " + token.value);
-                    }     
+                    } 
+                // to get the AST    
                 } else if (flag.equals("-ast")){
                     AST ast = parser.getAST(screenedTokens);
                     ast.traverseTree(ast.root, 0);
+                // to get the machine code. assembly code is dumped to the current directory as "asmfile"
                 } else if (flag.equals("-code")){
                     AST ast = parser.getAST(screenedTokens);
                     codegen.generateCode(ast);
